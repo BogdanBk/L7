de717e0e3261eb7c5f7819e2e6a6825d5d3be918
ump1love <steam.max22.d74.74@gmail.com>
2023-11-02 18:33:32 +0200

    first and second

---
+++ Repository/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Repository<int> repositoryInt = new Repository<int>();
+        Repository<double> repositoryDouble = new Repository<double>();
+
+        repositoryInt.AddToList(1);
+        repositoryInt.AddToList(2);
+        repositoryInt.AddToList(3);
+        repositoryDouble.AddToList(4.2);
+
+        Repository<int>.Criteria<int> criteria = repositoryInt.IsEven;
+
+
+        List<int> evenNumbers = repositoryInt.Find(criteria);
+
+        foreach (int number in evenNumbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        repositoryInt.Tmp();
+
+    }
+
+
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿class Repository<T>
+{
+    List<T> reposiory = new List<T>();
+    public delegate bool Criteria<T>(T item);
+
+    public List<T> Find(Criteria<T> criteria)
+    {
+        List<T> list = new List<T>();
+
+        foreach (T items in reposiory)
+        {
+            if (criteria(items))
+            {
+                list.Add(items);
+            }
+        }
+        return list;
+    }
+
+    public void AddToList(T item)
+    {
+        reposiory.Add(item);
+    }
+    public bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    public void Tmp()
+    {
+        for (int i = 0; i < reposiory.Count; i++) { Console.WriteLine(reposiory[i]); }
+    }
+}
